db62a9a3e7e1a5436635e3b1e86c06789b12a3aa
Ana Paula Silvestri <apsilvestri@gmail.com>
2021-10-14 21:42:47 -0300

    Implementado Loop While

---
+++ src/EntendendoWhile.java
@@ -0,0 +1,16 @@
+
+public class EntendendoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int vezes;
+		
+		vezes = 0;
+		while (vezes<3) {
+			System.out.println("Sono!!!");
+			vezes = vezes + 1;
+		}
+		
+	}
+
+}
--- src/LoopWhile.java
+++ src/LoopWhile.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
 
 public class LoopWhile {
 
 	public static void main(String[] args) {
 		/*Imagine que uma ONG precise saber a média de idades dos alunos de uma escola.
 		 * Sabendo de antemão quantos alunos existem e sem a necessidade de armazenar permanentemente a idade de cada um desses alunos, qual é a melhor forma de calcular essa média?*/
+		 Scanner leitor = new Scanner(System.in);
+		 
+		 int idade;
+		 double media=0;
+		 int numalunos=0;
+		 int quantidade;
+		 
+		 System.out.println("qtos alunos tem na turma?");
+		 quantidade = leitor.nextInt();
+		 
+		 while (numalunos < quantidade) {
+			 numalunos = numalunos + 1;
+			 System.out.println("Digite a idade aluno " + numalunos);
+			 idade = leitor.nextInt();
+			 media = media + idade;
+		 }
+		
+		 media = media / (double)numalunos;
+		 
+		 System.out.println(media);
+				 
+		leitor.close();		 
 
+		}
 	}
 
-}
+
